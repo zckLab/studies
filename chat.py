909d86ad04d476fb95c06a45773d192729212056
zcklab <otonielhenrique@icloud.com>
2024-03-29 10:03:14 -0300

    just study

---
+++ chat.py
@@ -0,0 +1,34 @@
+
+
+#isto é basicamente um codigo que faz o input do nome e da mensagem
+#deixando "nome - mensagem" no final.
+
+import os
+
+mensagens = []
+
+nome = "@" + input("name: ")
+
+while True:
+
+    #limpando terminal
+    os.system('cls')
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+            
+    print("__________")
+
+    #obtendo o texto
+    print("Digite fim para cancelar a operação")
+    texto = input("Mensagem: ")
+    if texto == "fim":
+        break
+
+
+  #adicionar mensagem na lista
+    mensagens.append({
+        "nome": nome,
+        "texto": texto
+    })
---
+++ studies/dict.py
@@ -0,0 +1,13 @@
+antoin = {
+"nome": "antoin",
+"idade": 22,
+"peso": 75, 
+"altura": 1.75,
+"estado atual": "desempregado",
+"renda mensal": "black hat",
+
+}
+
+#neste caso, o print chama a variavel e dps imprime o valor do elemento citado em aspas simples
+print (antoin['idade'])
+print (antoin['nome'])
