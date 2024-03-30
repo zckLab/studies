2a65b5143c5420ecaf7a81931b87ba061ccecf2a
zcklab <otonielhenrique@icloud.com>
2024-03-30 12:53:24 -0300

    java

--- java/App.java
+++ java/App.java
@@ -1,8 +1,8 @@
-package studies;
+package java;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("App");
+        System.out.println("Hello World!");
     }
 
 }
