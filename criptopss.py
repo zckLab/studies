882a6ed717e1897e3fb740f36401ff2e7612d709
zcklab <otonielhenrique@icloud.com>
2024-04-11 19:21:59 -0300

    studies only

---
+++ criptopss.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
+
+def main():
+    print("Bem-vindo ao gerador de senhas!")
+    while True:
+        try:
+            comprimento = int(input("Quantos caracteres você deseja em sua senha (entre 8 e 50)? "))
+            if comprimento < 8 or comprimento > 50:
+                print("Por favor, escolha um número entre 8 e 50.")
+            else:
+                senha = gerar_senha(comprimento)
+                print("Sua senha gerada é:", senha)
+                break
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
+if __name__ == "__main__":
+    main()
